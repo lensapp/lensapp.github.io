afa679c9cfc4d1a8cb30904b7529c9157fd151ce
Miska Kaipiainen <miska.kaipiainen@gmail.com>
2021-03-04 15:40:20 +0200

    update users to match latest stats

--- js/scripts.js
+++ js/scripts.js
@@ -389,7 +389,7 @@
     url: 'https://api.github.com/repos/lensapp/lens',
     success: function( data ){
       if(data.stargazers_count && data.forks){
-        $( "#happy-users-count" ).attr( 'data-count', Math.round(data.stargazers_count * 9.79) );
+        $( "#happy-users-count" ).attr( 'data-count', Math.round(data.stargazers_count * 10.92) );
         $( "#stargazers-count" ).attr( 'data-count', data.stargazers_count );
         $( "#forks-count" ).attr( 'data-count', data.forks );
         updateCounters();
